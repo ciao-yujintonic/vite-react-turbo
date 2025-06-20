ad0dec3236ce812aa7643a8548266f1a76b86cbd
ciao-yujintonic <yujin.hong420@gmail.com>
2025-06-20 20:10:47 +0900

    ✅ app build check

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,18 @@
 import { createRoot } from "react-dom/client";
-import { Header, Counter, Button, FeedbackDialog } from "@repo/ui";
-import "./style.css";
+import {
+  Counter,
+  Button,
+  FeedbackDialog,
+  IHeaderIcon,
+  HeaderActionMenus,
+  HeaderIconType,
+} from "@repo/ui";
+import { InfoOutlined, HelpOutline } from "@mui/icons-material";
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./style.css";
+
+const Qna = () => <div>QnA Page</div>;
 
 const App = () => {
   const [dialogOpen, setDialogOpen] = useState<boolean>(false);
@@ -10,26 +21,54 @@ const App = () => {
     setDialogOpen(false);
   };
 
+  const iconData: Array<IHeaderIcon> = [
+    {
+      svgIcon: InfoOutlined,
+      alt: "Manual",
+      tooltiptext: "Manual",
+      isShown: true,
+      type: HeaderIconType.LINK,
+      url: "http://www.google.com",
+    },
+    {
+      svgIcon: HelpOutline,
+      alt: "QnA",
+      tooltiptext: "QnA",
+      isShown: true,
+      type: HeaderIconType.LINK,
+      url: "/qna",
+    },
+  ];
   return (
-    <div>
-      <Header title="Web" />
-      <div className="card">
-        <Counter />
-        <Counter />
-      </div>
-      <Button
-        onClick={() => {
-          setDialogOpen((prev: boolean) => !prev);
-        }}
-      />
-      <FeedbackDialog
-        open={dialogOpen}
-        title="Feedback"
-        content={<div>Do you like this app?</div>}
-        onAction={handleDialogClose}
-        onClose={handleDialogClose}
-      />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <HeaderActionMenus iconData={iconData} />
+              <div className="card">
+                <Counter />
+                <Counter />
+              </div>
+              <Button
+                onClick={() => {
+                  setDialogOpen((prev: boolean) => !prev);
+                }}
+              />
+              <FeedbackDialog
+                open={dialogOpen}
+                title="Feedback"
+                content={<div>Do you like this app?</div>}
+                onAction={handleDialogClose}
+                onClose={handleDialogClose}
+              />
+            </div>
+          }
+        />
+        <Route path="/qna" element={<Qna />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
